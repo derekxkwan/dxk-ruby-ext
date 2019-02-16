1a86041d39b26743b7357d0e592e19f3694a5c12
Derek Kwan <derek.x.kwan@gmail.com>
2019-02-16 03:39:06 -0800

    Create dxk_array_ext.rb

---
+++ dxk_array_ext.rb
@@ -0,0 +1,39 @@
+class Array
+  def normalize
+    cur_max = self.max
+    return self.map{|i| i.to_f/cur_max}
+  end
+
+  def cdf
+    cur_sum = 0
+    return self.map{|i| cur_sum += i}.normalize
+  end
+
+  def wsample(weight_arr, num = 1)
+    cur_len = self.length
+    w_len = weight_arr.length
+    ret = []
+    num = (num > cur_len ? cur_len : (num < 1 ? 1 : num)).to_i
+
+    weight_arr = weight_arr + [0] * (cur_len - w_len) if w_len < cur_len
+    
+    normalized = weight_arr[0..cur_len].cdf
+    num.times do
+      idx = num -1
+      cur_rand = rand()
+      normalized.each_with_index do |val, i|
+        idx = i
+        if cur_rand < val
+          idx = i
+          break
+        end
+      end
+      ret =ret + [self[idx]]
+    end
+    if num <= 1
+      return ret[0]
+    else
+      return ret
+    end
+  end
+end
